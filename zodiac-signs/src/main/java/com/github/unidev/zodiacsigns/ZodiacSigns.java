268bbd6fa7576de9454165b8a7a510684010abbc
Denis O <denis@universal-development.com>
2017-11-23 21:21:20 +0200

    Added Zodiac Signs enum

---
+++ zodiac-signs/src/main/java/com/github/unidev/zodiacsigns/ZodiacSigns.java
@@ -0,0 +1,9 @@
+package com.github.unidev.zodiacsigns;
+
+/**
+ * Zodiac signs enum.
+ */
+public enum ZodiacSigns {
+    Capricorn, Aquarius, Pisces, Aries, Taurus, Gemini, Cancer, Leo, Virgo, Libra, Scorpio, Sagittarius
+
+}
