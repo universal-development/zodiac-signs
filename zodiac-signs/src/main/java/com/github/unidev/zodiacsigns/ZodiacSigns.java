b3865c3e27715652e447c6a1b204637b9e2a4bed
Denis O <denis@universal-development.com>
2017-11-23 21:32:11 +0200

    Added verifiers support for enum

--- zodiac-signs/src/main/java/com/github/unidev/zodiacsigns/ZodiacSigns.java
+++ zodiac-signs/src/main/java/com/github/unidev/zodiacsigns/ZodiacSigns.java
@@ -1,9 +1,24 @@
 package com.github.unidev.zodiacsigns;
 
+import java.time.LocalDate;
+import java.util.function.Predicate;
+
 /**
  * Zodiac signs enum.
  */
 public enum ZodiacSigns {
-    Capricorn, Aquarius, Pisces, Aries, Taurus, Gemini, Cancer, Leo, Virgo, Libra, Scorpio, Sagittarius
+    Capricorn(
+        date -> (date.getMonthValue() == 12 && date.getDayOfMonth() >= 22 && date.getDayOfMonth() <= 31),
+        date -> (date.getMonthValue() == 1 && date.getDayOfMonth() >= 1 && date.getDayOfMonth() <= 19)
+    ),
+
+    Aquarius, Pisces, Aries, Taurus, Gemini, Cancer, Leo, Virgo, Libra, Scorpio, Sagittarius;
+
+
+    private Predicate<LocalDate> verifiers[];
+
+    ZodiacSigns(Predicate<LocalDate>... verifiers) {
+        this.verifiers = verifiers;
+    }
 
 }
