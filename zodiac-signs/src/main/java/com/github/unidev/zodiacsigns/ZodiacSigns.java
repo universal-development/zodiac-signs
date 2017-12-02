f67a9a21bf1b3e7caef1c5421720bc5994786719
Denis O <denis@universal-development.com>
2017-12-02 15:58:39 +0200

    Updated formatting

--- zodiac-signs/src/main/java/com/github/unidev/zodiacsigns/ZodiacSigns.java
+++ zodiac-signs/src/main/java/com/github/unidev/zodiacsigns/ZodiacSigns.java
@@ -8,49 +8,73 @@ import java.util.function.Predicate;
  */
 public enum ZodiacSigns {
     Capricorn(
-        date -> (date.getMonthValue() == 12 && date.getDayOfMonth() >= 22 && date.getDayOfMonth() <= 31),
-        date -> (date.getMonthValue() == 1 && date.getDayOfMonth() >= 1 && date.getDayOfMonth() <= 19)
+        date -> (date.getMonthValue() == 12 && date.getDayOfMonth() >= 22
+            && date.getDayOfMonth() <= 31),
+        date -> (date.getMonthValue() == 1 && date.getDayOfMonth() >= 1
+            && date.getDayOfMonth() <= 19)
     ),
 
-    Aquarius (
-        date -> (date.getMonthValue() == 1 && date.getDayOfMonth() >= 20 && date.getDayOfMonth() <= 31),
-        date -> (date.getMonthValue() == 2 && date.getDayOfMonth() >= 1 && date.getDayOfMonth() <= 17)
+    Aquarius(
+        date -> (date.getMonthValue() == 1 && date.getDayOfMonth() >= 20
+            && date.getDayOfMonth() <= 31),
+        date -> (date.getMonthValue() == 2 && date.getDayOfMonth() >= 1
+            && date.getDayOfMonth() <= 17)
     ),
 
     Pisces(
-        date -> (date.getMonthValue() == 2 && date.getDayOfMonth() >= 18 && date.getDayOfMonth() <= 29),
-        date -> (date.getMonthValue() == 3 && date.getDayOfMonth() >= 1 && date.getDayOfMonth() <= 19)
+        date -> (date.getMonthValue() == 2 && date.getDayOfMonth() >= 18
+            && date.getDayOfMonth() <= 29),
+        date -> (date.getMonthValue() == 3 && date.getDayOfMonth() >= 1
+            && date.getDayOfMonth() <= 19)
     ),
     Aries(
-        date -> (date.getMonthValue() == 3 && date.getDayOfMonth() >= 20 && date.getDayOfMonth() <= 31),
-        date -> (date.getMonthValue() == 4 && date.getDayOfMonth() >= 1 && date.getDayOfMonth() <= 19)
+        date -> (date.getMonthValue() == 3 && date.getDayOfMonth() >= 20
+            && date.getDayOfMonth() <= 31),
+        date -> (date.getMonthValue() == 4 && date.getDayOfMonth() >= 1
+            && date.getDayOfMonth() <= 19)
     ),
     Taurus(
-        date -> (date.getMonthValue() == 4 && date.getDayOfMonth() >= 20 && date.getDayOfMonth() <= 30),
-        date -> (date.getMonthValue() == 5 && date.getDayOfMonth() >= 1 && date.getDayOfMonth() <= 20)
+        date -> (date.getMonthValue() == 4 && date.getDayOfMonth() >= 20
+            && date.getDayOfMonth() <= 30),
+        date -> (date.getMonthValue() == 5 && date.getDayOfMonth() >= 1
+            && date.getDayOfMonth() <= 20)
     ),
     Gemini(
-        date -> (date.getMonthValue() == 5 && date.getDayOfMonth() >= 21 && date.getDayOfMonth() <= 31),
-        date -> (date.getMonthValue() == 6 && date.getDayOfMonth() >= 1 && date.getDayOfMonth() <= 20)
+        date -> (date.getMonthValue() == 5 && date.getDayOfMonth() >= 21
+            && date.getDayOfMonth() <= 31),
+        date -> (date.getMonthValue() == 6 && date.getDayOfMonth() >= 1
+            && date.getDayOfMonth() <= 20)
     ),
     Cancer(
-        date -> (date.getMonthValue() == 6 && date.getDayOfMonth() >= 21 && date.getDayOfMonth() <= 30),
-        date -> (date.getMonthValue() == 7 && date.getDayOfMonth() >= 1 && date.getDayOfMonth() <= 22)
-    ),Leo(
-        date -> (date.getMonthValue() == 7 && date.getDayOfMonth() >= 23 && date.getDayOfMonth() <= 31),
-        date -> (date.getMonthValue() == 8 && date.getDayOfMonth() >= 1 && date.getDayOfMonth() <= 22)
+        date -> (date.getMonthValue() == 6 && date.getDayOfMonth() >= 21
+            && date.getDayOfMonth() <= 30),
+        date -> (date.getMonthValue() == 7 && date.getDayOfMonth() >= 1
+            && date.getDayOfMonth() <= 22)
+    ), Leo(
+        date -> (date.getMonthValue() == 7 && date.getDayOfMonth() >= 23
+            && date.getDayOfMonth() <= 31),
+        date -> (date.getMonthValue() == 8 && date.getDayOfMonth() >= 1
+            && date.getDayOfMonth() <= 22)
     ), Virgo(
-        date -> (date.getMonthValue() == 8 && date.getDayOfMonth() >= 23 && date.getDayOfMonth() <= 31),
-        date -> (date.getMonthValue() == 9 && date.getDayOfMonth() >= 1 && date.getDayOfMonth() <= 22)
+        date -> (date.getMonthValue() == 8 && date.getDayOfMonth() >= 23
+            && date.getDayOfMonth() <= 31),
+        date -> (date.getMonthValue() == 9 && date.getDayOfMonth() >= 1
+            && date.getDayOfMonth() <= 22)
     ), Libra(
-        date -> (date.getMonthValue() == 9 && date.getDayOfMonth() >= 23 && date.getDayOfMonth() <= 30),
-        date -> (date.getMonthValue() == 10 && date.getDayOfMonth() >= 1 && date.getDayOfMonth() <= 22)
+        date -> (date.getMonthValue() == 9 && date.getDayOfMonth() >= 23
+            && date.getDayOfMonth() <= 30),
+        date -> (date.getMonthValue() == 10 && date.getDayOfMonth() >= 1
+            && date.getDayOfMonth() <= 22)
     ), Scorpio(
-        date -> (date.getMonthValue() == 10 && date.getDayOfMonth() >= 23 && date.getDayOfMonth() <= 31),
-        date -> (date.getMonthValue() == 11 && date.getDayOfMonth() >= 1 && date.getDayOfMonth() <= 21)
+        date -> (date.getMonthValue() == 10 && date.getDayOfMonth() >= 23
+            && date.getDayOfMonth() <= 31),
+        date -> (date.getMonthValue() == 11 && date.getDayOfMonth() >= 1
+            && date.getDayOfMonth() <= 21)
     ), Sagittarius(
-        date -> (date.getMonthValue() == 11 && date.getDayOfMonth() >= 22 && date.getDayOfMonth() <= 30),
-        date -> (date.getMonthValue() == 12 && date.getDayOfMonth() >= 1 && date.getDayOfMonth() <= 21)
+        date -> (date.getMonthValue() == 11 && date.getDayOfMonth() >= 22
+            && date.getDayOfMonth() <= 30),
+        date -> (date.getMonthValue() == 12 && date.getDayOfMonth() >= 1
+            && date.getDayOfMonth() <= 21)
     );
 
 
