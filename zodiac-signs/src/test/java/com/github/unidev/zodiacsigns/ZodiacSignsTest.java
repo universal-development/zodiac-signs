cf3d9fe39e0f4debb7302203896b108ee45b1fab
Denis O <denis@universal-development.com>
2017-12-02 16:40:17 +0200

    Added test for matching

--- zodiac-signs/src/test/java/com/github/unidev/zodiacsigns/ZodiacSignsTest.java
+++ zodiac-signs/src/test/java/com/github/unidev/zodiacsigns/ZodiacSignsTest.java
@@ -1,13 +1,15 @@
 package com.github.unidev.zodiacsigns;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 
 public class ZodiacSignsTest {
 
     @Test
-    public void testDateMatch() {
-
-
+    public void testLeoMatching() {
+        assertTrue(ZodiacSigns.Leo.match(7, 25));
+        assertFalse(ZodiacSigns.Leo.match(7, 19));
     }
-
 }
